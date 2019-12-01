9bce688e1b00db2aebe28c48a58a3b4b3c4429f4
Bilal <bilalusuf93@hotmail.com>
2019-12-01 21:40:40 +0500

    i have added a class named Demo

---
+++ DesktopApp1/Demo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopApp1
+{
+    class Demo
+    {
+        public int Id { get; set; }
+    }
+}
